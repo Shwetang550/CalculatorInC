427e990e17ee6dc30724b2a4aef4a34e0dca1976
Shwetang <shwetangsingh5@gmail.com>
2021-01-21 01:05:09 +0530

    font awesome icon added

--- todo-list-app/src/Components/listItems.jsx
+++ todo-list-app/src/Components/listItems.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const ListItems = (props) => {
     const { text } = props;
-    return (<li> {text} </li> );
+    
+    return (
+        <>
+            <div className="todo_style" style={{marginLeft: "1rem"}}>
+                <i className="fa fa-times" aria-hidden="true" />
+                <li> {text} </li>
+            </div>
+        </>
+    );
 }
  
 export default ListItems;
