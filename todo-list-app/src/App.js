f0f9692c4e1674113c938de5f087dd6d14ddf8e2
Shwetang <shwetangsingh5@gmail.com>
2021-01-21 00:14:06 +0530

    list items update

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 
 const App = () => {
   let [inputList, setInputList] = useState("");
+  let [items, setItems] = useState([]);
 
   const inputItem = (event) => { 
     setInputList(event.target.value);
+  };
+
+  const addItem = () => { 
+    setItems((prevItems) => {
+      return [...prevItems, inputList];
+    })
   };
 
   return ( 
@@ -15,10 +22,14 @@ const App = () => {
           <h1> ToDo List App</h1>
           <br />
           <input type="text" placeholder="Add Items" onChange={inputItem} />
-          <button> + </button>
+          <button onClick={addItem}> + </button>
           
           <ol>
-            <li> {inputList} </li>
+            {
+              items.map((item) => {
+                return <li> {item} </li>;
+              })
+            }
           </ol>
         </div>
       </div>
