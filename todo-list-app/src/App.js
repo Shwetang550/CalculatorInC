43345386cd36584f8974114076f264eec9313a0c
Shwetang <shwetangsingh5@gmail.com>
2021-01-20 23:58:59 +0530

    list update

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  let [inputList, setInputList] = useState("");
+
+  const inputItem = (event) => { 
+    setInputList(event.target.value);
+  };
+
   return ( 
     <>
       <div className="main_div">
@@ -8,11 +14,11 @@ const App = () => {
           <br />
           <h1> ToDo List App</h1>
           <br />
-          <input type="text" placeholder="Add Items" />
+          <input type="text" placeholder="Add Items" onChange={inputItem} />
           <button> + </button>
           
           <ol>
-            <li>apple</li>
+            <li> {inputList} </li>
           </ol>
         </div>
       </div>
