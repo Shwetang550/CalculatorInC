d929e4fd0accaa5db4eeb96c071ae8362e6eed99
Shwetang <shwetangsingh5@gmail.com>
2021-01-24 10:49:43 +0530

    material-ui/icons installed

--- src/Components/ToDoMaterialUI.jsx
+++ src/Components/ToDoMaterialUI.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 
 const ToDoMaterialUI = () => {
     return ( 
@@ -9,7 +11,10 @@ const ToDoMaterialUI = () => {
                     <h1>ToDo List</h1>
                     <br />
 
-                    <input type="text" placeholder="Add items"/>
+                    <input type="text" placeholder="Add items" />
+                    <Button>
+                        <AddIcon fontSize="large"/>
+                    </Button>
                 </div>
             </div>
         </>
