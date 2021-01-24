1c1eebfef1da3868c7769a25604801d968f38cd5
Shwetang <shwetangsingh5@gmail.com>
2021-01-24 10:31:50 +0530

    materialUi component added

---
+++ src/Components/ToDoMaterialUI.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDoMaterialUI = () => {
+    return ( 
+        <>
+            <div>
+                <h1>ToDoMaterialUI</h1>
+            </div>
+            </>
+     );
+}
+ 
+export default ToDoMaterialUI;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import ToDoMaterialUI from './Components/ToDoMaterialUI';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDoMaterialUI />
   </React.StrictMode>,
   document.getElementById('root')
 );
