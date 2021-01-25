5667183e30fb640e443468ad64d56ab55af2839b
Shwetang <shwetangsingh5@gmail.com>
2021-01-25 23:33:11 +0530

    input item change

--- src/Components/ToDoMaterialUI.jsx
+++ src/Components/ToDoMaterialUI.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 
 const ToDoMaterialUI = () => {
+    let [item, setItem] = useState("");
+
+    const handleInput = (event) => {
+        setItem(event.target.value);
+    }
+
     return ( 
         <>
             <div className="main_div">
@@ -11,14 +17,14 @@ const ToDoMaterialUI = () => {
                     <h1>ToDo List</h1>
                     <br />
 
-                    <input type="text" placeholder="Add items" />
+                    <input type="text" placeholder="Add items" onChange={handleInput} />
                     <Button className="materialBtn"> 
                         <AddIcon fontSize="large"/>
                     </Button>
 
                     <br />
                     <ol>
-                        <li>Buy an apple</li>
+                        <li>{item}</li>
                     </ol>
                     <br />
                 </div>
